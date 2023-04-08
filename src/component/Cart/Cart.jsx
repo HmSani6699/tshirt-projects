3bd9f8f9771996adcadb17df57aef33bd556e772
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-04-09 01:36:37 +0600

    ad to rect tostify

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
-    console.log(cart)
     return (
         <div>
             <h2>Order summary : {cart.length}</h2>
             {
-                cart.map(ct => <p
-                    key={ct._id}
-                >{ct.name}
+                cart.map(ct => <p>{ct.name}
                     <button onClick={() => handleRemoveFromCart(ct._id)}>X</button>
                 </p>)
             }
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -3,6 +3,8 @@ import { useLoaderData } from 'react-router-dom';
 import Cart from '../Cart/Cart';
 import TShirt from '../TShirt/TShirt';
 import './Home.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Home = () => {
     const tShirts = useLoaderData();
@@ -10,12 +12,28 @@ const Home = () => {
     const [cart, setCart] = useState([]);
 
     const handleAdToCart = tShirt => {
-        const newCart = [...cart, tShirt]
-        setCart(newCart)
+        const exists = cart.find(crt => crt._id === tShirt._id);
+        if (exists) {
+            toast.warn('🦄 Already add!', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+        }
+        else {
+            const newCart = [...cart, tShirt]
+            setCart(newCart)
+        }
+
     }
     const handleRemoveFromCart = (id) => {
-        const exists = cart.filter(ct => ct._id !== id);
-        setCart(exists)
+        const getCart = cart.filter(ct => ct._id !== id);
+        setCart(getCart)
     }
 
     return (
@@ -35,6 +53,7 @@ const Home = () => {
                     handleRemoveFromCart={handleRemoveFromCart}
                 ></Cart>
             </div>
+            <ToastContainer />
         </div>
     );
 };
