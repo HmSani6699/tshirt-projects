5761abf53816cf5131abdf2196bd431a9fd84b71
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-04-09 00:03:40 +0600

    load to tshirt data

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const tShirts = useLoaderData();
+    console.log(tShirts)
     return (
         <div>
             <h2>this is home</h2>
