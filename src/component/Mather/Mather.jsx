c8a608e783f63b052b4eb304390675478c7172a5
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-04-09 12:56:29 +0600

    ad to mother component

--- src/component/Mather/Mather.jsx
+++ src/component/Mather/Mather.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Mama from '../Mama/Mama';
+import Nana from '../Nana/Nana';
+import Aunty from '../Aunty/Aunty';
 
 const Mather = () => {
     return (
         <div>
             <h2>Mather</h2>
+            <section className='flex'>
+                <Mama></Mama>
+                <Nana></Nana>
+                <Aunty></Aunty>
+            </section>
         </div>
     );
 };
