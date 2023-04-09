a523c69daa1c73a510e20c5a0cc546a8ca92ad7e
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-04-09 15:44:15 +0600

    ad to new context part

--- src/component/Father/Father.jsx
+++ src/component/Father/Father.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import Sister from '../Sister/Sister';
 import Bother from '../Bother/Bother';
 import ME from '../ME/ME';
 
+export const AuthorFather = createContext(0);
+
 const Father = ({ ruing }) => {
+    const [amount, setAmount] = useState(0)
     return (
         <div>
             <h2>Father</h2>
-            <section className='flex'>
-                <ME></ME>
-                <Sister></Sister>
-                <Bother ruing={ruing}></Bother>
-            </section>
+            <p>Father amount :{amount}</p>
+            <AuthorFather.Provider value={[amount, setAmount]}>
+                <section className='flex'>
+                    <ME></ME>
+                    <Sister></Sister>
+                    <Bother ruing={ruing}></Bother>
+                </section>
+            </AuthorFather.Provider>
         </div>
     );
 };
--- src/component/ME/ME.jsx
+++ src/component/ME/ME.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthorFather } from '../Father/Father';
 
 const ME = () => {
+    const [amount, setAmount] = useContext(AuthorFather);
     return (
         <div>
             <h2> ISMe</h2>
+            <p>Amount{amount}</p>
+            <button onClick={() => setAmount(amount + 100)}>Increment</button>
         </div>
     );
 };
